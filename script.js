bbe96bab5e28b193276d0418abe14b0dae8851d8
Shady Israel <drsheko91@gmail.com>
2022-03-11 10:31:07 -0600

    add change div background feature on click

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const container = document.querySelector('.container');
 
-/*const para = document.createElement("div");
-container.appendChild(para)
-para.textContent='shady'*/
 for( var i =0 ; i<256 ; i++ ){
  const div = document.createElement('div');
-    div.classList.add('divGrid')
-    container.appendChild(div);
-    
-   
-
+    div.classList.add('divGrid');
+    //div.classList.add('permanentHover');
+    container.appendChild(div);  
 
 }
-//div.textContent='shady'
+const divs = document.querySelectorAll('.divGrid')
+for (const div of divs){
+div.addEventListener('click',()=>{div.classList.add('permanentHover')})
+}
