ca1b38a888a7b22f4ca0f4e2b6fcd0a946dfe6d9
Shady Israel <drsheko91@gmail.com>
2022-03-12 01:33:53 -0600

    add buttons and control size of pixels in grid

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 const container = document.querySelector('.container');
-
-for( var i =0 ; i<256 ; i++ ){
+let num = prompt('enter num:')
+for( var i =0 ; i<num ; i++ ){
  const div = document.createElement('div');
     div.classList.add('divGrid');
-    //div.classList.add('permanentHover');
-    container.appendChild(div);  
+    container.appendChild(div);
+    for (var j =0 ; j<num ; j++){
+        const column = document.createElement('div');
+        column.classList.add('divColumn');
+        div.appendChild(column);
+    }  
 
 }
-const divs = document.querySelectorAll('.divGrid')
-for (const div of divs){
-div.addEventListener('click',()=>{div.classList.add('permanentHover')})
+const columns = document.querySelectorAll('.divColumn')
+for (const column of columns){
+column.addEventListener('click',()=>{column.classList.add('permanentHover')})
 }
+
+const clear = document.querySelector('.clear')
+for (const column of columns){
+clear.addEventListener('click',()=>{column.classList.remove('permanentHover')})}
+
+const erase = document.querySelector('.erase')
+erase.addEventListener('click',()=>{column.classList.remove('permanentHover')})
+
+
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; 
+
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+   
+}
+
+
